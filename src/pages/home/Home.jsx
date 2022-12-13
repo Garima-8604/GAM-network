3870f6a8fcc664699c0c8062daded6b763cfb089
Maithri Muralikrishna <m.muralikrishna.cse21@itbhu.ac.in>
2022-12-13 23:37:21 +0530

    Added PostShare functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { PostSide } from '../../components/PostSide/PostSide'
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import './Home.css'
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide/>
+        <PostSide/>
         <div className="postSide">Posts</div>
         <div className="RightSide">Rightside</div>
 
